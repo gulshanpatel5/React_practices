756a1f91ec024a2a32e6a4a30e856aa4dec1ffe3
gulshanpatel5 <gulshanpatel044@gmail.com>
2024-12-19 11:55:52 +0530

    almost created a counter website

--- counting/src/App.jsx
+++ counting/src/App.jsx
@@ -1,17 +1,37 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+// import  viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
+  const [counter, setCounter] = useState(15) // this is the main feature of react where we can update the counter value in one place it automatically update every where.
+
+
+  // let counter = 15;
+  const addValue = ()=> {
+    console.log("clicked");
+    
+    //counter = counter + 1
+    setCounter (counter + 1)
+    
+  }
+  const removevalue = () =>{
+    setCounter (counter - 1)
+
+  }
 
   return (
     <>
       <h1>Chai with Gulshan</h1>
-      <h2>Counter Value: 5</h2>
-      <button>Add value</button>
-      <button>Add value</button>
+      <h2>Counter Value: {counter}</h2>
+
+      <button 
+      onClick={addValue}
+      >Add value</button>
+      <br/>
+      <button
+      onClick={removevalue}>decrees value</button>
     </>
   )
 }
